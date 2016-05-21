0dcda391c92f0ffa82f501b98ece02aba653d408
liyan <871797131@qq.com>
2016-05-21 13:55:00 +0800

    init

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+//mysql database address
+define('DB_HOST','127.0.0.1');
+//mysql database user
+define('DB_USER','root');
+//database password
+define('DB_PASSWD','123456');
+//database name
+define('DB_NAME','blog');
+//database prefix
+define('DB_PREFIX','ly_');
+//auth key
+define('AUTH_KEY','9jOWGX%T%Ojv8xEa9fJ$R0Rw%!hsbcW4ea0ce3fb577f1a572dc96f65ff0794b7');
+//cookie name
+define('AUTH_COOKIE_NAME','EM_AUTHCOOKIE_5GHeKAWX0OtnUgJmfVTwOGadaCBTSLcf');
